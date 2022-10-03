2d1aa21ce0f25f68d498d2524a3c69650fb46dbe
Okyere Amponsah Kwatia <77788839+orionty@users.noreply.github.com>
2022-10-02 18:43:27 -0700

    Updates

---
+++ precache-manifest.32d87ee03eebd24f5e743ec18c5dd9bc.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "13be6719c5d0d7175a3c",
+    "url": "/elearning/static/css/main.9c747e4b.chunk.css"
+  },
+  {
+    "revision": "13be6719c5d0d7175a3c",
+    "url": "/elearning/static/js/main.e9e66d4c.chunk.js"
+  },
+  {
+    "revision": "a5f37c5a88c1d9d87b54",
+    "url": "/elearning/static/js/runtime~main.57abe32b.js"
+  },
+  {
+    "revision": "fc4aeab7ef19fdd6a4a9",
+    "url": "/elearning/static/js/2.875c77d9.chunk.js"
+  },
+  {
+    "revision": "737463e817372473c57b",
+    "url": "/elearning/static/js/3.c725dfe6.chunk.js"
+  },
+  {
+    "revision": "5e4e6857595f706c44db444c6fccb22b",
+    "url": "/elearning/static/media/RotationOfARigidObject.5e4e6857.pdf"
+  },
+  {
+    "revision": "d4f22d85fb77689f351510c7067e8740",
+    "url": "/elearning/static/media/MotionInOneDimension.d4f22d85.pdf"
+  },
+  {
+    "revision": "0e3002b6005c06c66587e697fc02fd39",
+    "url": "/elearning/static/media/Vectors.0e3002b6.pdf"
+  },
+  {
+    "revision": "76c7a52d4762b92be6326d95d8c535d6",
+    "url": "/elearning/static/media/MotionInTwoDimensions.76c7a52d.pdf"
+  },
+  {
+    "revision": "cd90a3c0e38191b94b0144ac6f54de26",
+    "url": "/elearning/static/media/LawsOfMotion.cd90a3c0.pdf"
+  },
+  {
+    "revision": "a36e7168d8d285ea30b50ad97b356484",
+    "url": "/elearning/static/media/Application'sOfNewtonLaws.a36e7168.pdf"
+  },
+  {
+    "revision": "60f11382d8540641ad11d1281e15ea6a",
+    "url": "/elearning/static/media/WorkAndKineticEnergy.60f11382.pdf"
+  },
+  {
+    "revision": "df5d7442d95fbda871f55d5c58eac52c",
+    "url": "/elearning/static/media/PotentialEnergyAndConservationOfEnergy.df5d7442.pdf"
+  },
+  {
+    "revision": "03e686447a5019540b8dae6a541f2e5a",
+    "url": "/elearning/static/media/LinearMomentumAndCollisions.03e68644.pdf"
+  },
+  {
+    "revision": "4ac531ffa963e8e91fedb30dce9948b7",
+    "url": "/elearning/static/media/PhysicsAndMeasurement.4ac531ff.pdf"
+  },
+  {
+    "revision": "1347a402bf9608628c98a0da782828bf",
+    "url": "/elearning/static/media/RollingAndAngularMomentum.1347a402.pdf"
+  },
+  {
+    "revision": "bedeaf8f7e72cb62de65e6a131830d48",
+    "url": "/elearning/static/media/StaticEquilibriumAndElasticity.bedeaf8f.pdf"
+  },
+  {
+    "revision": "d04012c757165762be46f8b50ffed521",
+    "url": "/elearning/static/media/OscillatoryMotion.d04012c7.pdf"
+  },
+  {
+    "revision": "4758db53fdc4ea408cfde85b802ca4ca",
+    "url": "/elearning/static/media/TheLawOfGravity.4758db53.pdf"
+  },
+  {
+    "revision": "a19c2d5e4e224ef411c0a54ab31dff86",
+    "url": "/elearning/static/media/FluidMechanics.a19c2d5e.pdf"
+  },
+  {
+    "revision": "e0e5f0229f63edd5dc7668b751aa1c18",
+    "url": "/elearning/static/media/WaveMotion.e0e5f022.pdf"
+  },
+  {
+    "revision": "bba3a633f53c4da72568b1134a482106",
+    "url": "/elearning/static/media/SoundWaves.bba3a633.pdf"
+  },
+  {
+    "revision": "baad87384a5cbbaea4db68a7cac29d3d",
+    "url": "/elearning/static/media/SuperpositionAndStandingWaves.baad8738.pdf"
+  },
+  {
+    "revision": "75204b5ca1a037eb89a042e2915c23e1",
+    "url": "/elearning/static/media/Temperature.75204b5c.pdf"
+  },
+  {
+    "revision": "f7bf5fb3549278b5f8ae304fcf36e2bd",
+    "url": "/elearning/static/media/HeatAndTheFirstLawOfThermodynamics.f7bf5fb3.pdf"
+  },
+  {
+    "revision": "75acecf2f73ba9a4a71c369c68eb4719",
+    "url": "/elearning/static/media/TheKineticTheoryOFGases.75acecf2.pdf"
+  },
+  {
+    "revision": "8e89e31fa613f3e7b9a1f182f522ea59",
+    "url": "/elearning/static/media/HeatEnginesEntropyAndTheSecondLawOfThermodynamics.8e89e31f.pdf"
+  },
+  {
+    "revision": "1b5dfcd64108b3166bb72ba95eaa4a5f",
+    "url": "/elearning/static/media/ElectricFields.1b5dfcd6.pdf"
+  },
+  {
+    "revision": "284bfc679d1dcaca9b2feac2f1d112d0",
+    "url": "/elearning/static/media/GaussLaw.284bfc67.pdf"
+  },
+  {
+    "revision": "a0b241f92b6c3e589b27830b9874f4bc",
+    "url": "/elearning/static/media/ElectricPotential.a0b241f9.pdf"
+  },
+  {
+    "revision": "8db9e0e6e3db48f336d21063104275b3",
+    "url": "/elearning/static/media/CapacitanceAndDielectrics.8db9e0e6.pdf"
+  },
+  {
+    "revision": "cb2b3ff6f0494b1d9c72a5eac2683a7e",
+    "url": "/elearning/static/media/CurrentAndResistance.cb2b3ff6.pdf"
+  },
+  {
+    "revision": "334c8e344ce1e6d6f42b7d7c1211d36d",
+    "url": "/elearning/static/media/DirectCurrentCircuits.334c8e34.pdf"
+  },
+  {
+    "revision": "5d3c456dc4683788233b230f50cc810e",
+    "url": "/elearning/static/media/MagneticFields.5d3c456d.pdf"
+  },
+  {
+    "revision": "bf367687b54fefedab4fdd062181ab64",
+    "url": "/elearning/static/media/SourcesOfTheMagneticField.bf367687.pdf"
+  },
+  {
+    "revision": "8331cfeef903945b0bb87b6bd27d92d7",
+    "url": "/elearning/static/media/FaradaysLaw.8331cfee.pdf"
+  },
+  {
+    "revision": "f5818d252aa269dba4d6aae4774ad559",
+    "url": "/elearning/static/media/Inductance.f5818d25.pdf"
+  },
+  {
+    "revision": "6a631057a2f0590196f04802b62f4300",
+    "url": "/elearning/static/media/AlternatingCurrentCircuits.6a631057.pdf"
+  },
+  {
+    "revision": "55e0bb4961ffb45345d874d85c50f0ab",
+    "url": "/elearning/static/media/ElectromagneticWaves.55e0bb49.pdf"
+  },
+  {
+    "revision": "0af552b2218a138efe6fbc27ebe1061d",
+    "url": "/elearning/static/media/EssentialIdeas.0af552b2.pdf"
+  },
+  {
+    "revision": "f665074935e28ebd634a25dd5140e789",
+    "url": "/elearning/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/elearning/precache-manifest.75578ee671c8b832f1b61e6354a2cfb5.js"
+  "/elearning/precache-manifest.32d87ee03eebd24f5e743ec18c5dd9bc.js"
 );
 
 workbox.clientsClaim();
